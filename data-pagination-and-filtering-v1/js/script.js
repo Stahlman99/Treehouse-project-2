4e76addf60698ec63f019a05c7e691107860aed3
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-09-16 13:14:54 -0500

    Added the showPage function to the script.
    I added a function that displays the list items to a page when called.

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -10,7 +10,7 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
+let itemsPerPage = 9;
 
 
 /*
@@ -18,7 +18,35 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+// This function loops through the data objects and lists specified properties as seperate elements on the page.
+function showPage(list, page) {
 
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+
+   studentList = document.querySelector('ul.student-list');
+   studentList.innerHTML = '';
+
+   for ( let i = 0; i < list.length; i++) {
+      if ( i >= startIndex && i < endIndex ) {
+         let studentItem = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src=${list[i].picture.thumbnail} alt="Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${list[i].registered.date}</span>
+               </div>
+            </li>
+         `;
+         studentList.insertAdjacentHTML('beforeend', studentItem);
+      }
+   }
+}
+
+showPage(data, 3);
 
 /*
 Create the `addPagination` function

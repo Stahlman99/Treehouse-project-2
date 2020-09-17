b4f52f897274bf978d29b5693761b446eeef36db
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-09-16 20:21:02 -0500

    Added the addPagination function and comments.

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -10,6 +10,8 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
+
+// Specifies the # of items per page. It is declared here to make it easier to update the number.
 let itemsPerPage = 9;
 
 
@@ -18,13 +20,14 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-// This function loops through the data objects and lists specified properties as seperate elements on the page.
+// This function loops through the data objects and lists their data as seperate elements on the page.
+// It only shows a specified # of items per page, set in the itemsPerPage variable.
 function showPage(list, page) {
 
    let startIndex = (page * itemsPerPage) - itemsPerPage;
    let endIndex = page * itemsPerPage;
 
-   studentList = document.querySelector('ul.student-list');
+   const studentList = document.querySelector('ul.student-list');
    studentList.innerHTML = '';
 
    for ( let i = 0; i < list.length; i++) {
@@ -46,13 +49,35 @@ function showPage(list, page) {
    }
 }
 
-showPage(data, 3);
-
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+// This function creates the buttons required for selecting new pages, then adds an event listener to call the showPage function and provide the correct page number.
+function addPagination(list) {
+   const numberOfPages = Math.ceil(list.length / itemsPerPage);
 
+   let linkList = document.querySelector('ul.link-list');
+   linkList.innerHTML = '';
+
+   for (let i = 0; i < numberOfPages; i++) {
+      let buttonHTML = `<li><button type="button">${i+1}</button></li>`;
+      linkList.insertAdjacentHTML('beforeend', buttonHTML);
+   }
+
+   linkList.firstElementChild.className = 'active';
+
+   linkList.addEventListener('click', (e) => {
+
+      if ( e.target.tagName === 'BUTTON' ){
+         document.querySelector('.active').className = '';
+         e.target.className = 'active';
+      }
+      showPage(data, e.target.textContent);
+   });
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
